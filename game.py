0ad6308a75b3940871536fb6e5ca4d000005721b
Aditya <contactaditya832126@gmail.com>
2024-11-20 01:33:40 +0530

    Changed UI to improve the dimension of the court. Added functions for player movement

--- game.py
+++ game.py
@@ -21,73 +21,82 @@ GRAY = (200, 200, 200)
 # Net border
 DARK_GRAY = (70, 70, 70)
 
-NET_HEIGHT = 310
+NET_HEIGHT = 360
 
 
 class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("TENNIS 2D")
-        self.window_size = (800, 650)
+        self.window_size = (800, 720)
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode(self.window_size)
-        self.create_ground(screen=self.screen)
+        
         
         # Pre-render the court
-        self.court_surface = pygame.Surface((400, 580))
+        self.court_surface = pygame.Surface((400, 720))
         self.create_court()
-        self.court = pygame.Surface((400, 580), pygame.SRCALPHA)
+        self.court = pygame.Surface((400, 720), pygame.SRCALPHA)
+        self.create_ground(screen=self.screen)
         
         # Initialize the net
-        self.net = pygame.Surface((800, 20), pygame.SRCALPHA)
         self.create_net()
 
         # Players and Ball
-        self.user = Player(screen=self.court, left=350, top=530, width=50, height=50)
-        self.other = Player(screen=self.court, left=0, top=0, width=50, height=50)
+        self.user = Player(screen=self.court, player="user", left=380, top=660, width=20, height=40)
+        self.other = Player(screen=self.court, player= "other", left=0, top=0, width=20, height=40)
         self.ball = Ball(screen=self.court, left=250, top=250, width=20, height=20, x_pos=250, y_pos=250)
 
     def create_ground(self, screen, *args):
-        if not args:
-            for _ in range(300000):  # Adjust the number of dots
-                x = random.randint(0, 800)  # Random x position
-                y = random.randint(0, 650)  # Random y position
-                color = random.choice([DARK_GREEN, LIGHT_GREEN, GREEN])
-                pygame.draw.circle(screen, color, (x, y), random.randint(1, 2))
-        else:
-            for _ in range(10000):
-                x = random.randint(args[0], args[0] + args[2])
-                y = random.randint(args[1], args[1] + args[3])
-                color = random.choice([DARK_GREEN, LIGHT_GREEN, GREEN])
-                pygame.draw.circle(screen, color, (x, y), random.randint(1, 2))
+        for _ in range(300000):  # Adjust the number of dots
+            x = random.randint(0, 800)  # Random x position
+            y = random.randint(0, 720)  # Random y position
+            color = random.choice([DARK_GREEN, LIGHT_GREEN, GREEN])
+            pygame.draw.circle(screen, color, (x, y), random.randint(1, 2))
 
     def create_court(self):
         # Render the court gradient
-        for y in range(580):
+        for y in range(0, 720):
             color = (
-                COURT_BLUE_LIGHT[0] + (COURT_BLUE_DARK[0] - COURT_BLUE_LIGHT[0]) * (y / 580),
-                COURT_BLUE_LIGHT[1] + (COURT_BLUE_DARK[1] - COURT_BLUE_LIGHT[1]) * (y / 580),
-                COURT_BLUE_LIGHT[2] + (COURT_BLUE_DARK[2] - COURT_BLUE_LIGHT[2]) * (y / 580)
+                COURT_BLUE_LIGHT[0] + (COURT_BLUE_DARK[0] - COURT_BLUE_LIGHT[0]) * (y / 720),
+                COURT_BLUE_LIGHT[1] + (COURT_BLUE_DARK[1] - COURT_BLUE_LIGHT[1]) * (y / 720),
+                COURT_BLUE_LIGHT[2] + (COURT_BLUE_DARK[2] - COURT_BLUE_LIGHT[2]) * (y / 720)
             )
             pygame.draw.line(self.court_surface, color, (0, y), (400, y))
 
         # Draw court lines
-        pygame.draw.rect(self.court_surface, WHITE, (0, 0, 400, 580), 5)  # Court boundary
-        pygame.draw.line(self.court_surface, WHITE, (70, 0), (70, 580), 5)  # Singles Sideline
-        pygame.draw.line(self.court_surface, WHITE, (330, 0), (330, 580), 5)  # Singles Sideline
-        pygame.draw.line(self.court_surface, WHITE, (70, 145), (330, 145), 5)  # Service line
-        pygame.draw.line(self.court_surface, WHITE, (70, 435), (330, 435), 5)  # Service line
-        pygame.draw.line(self.court_surface, WHITE, (200, 145), (200, 435), 5)  # Centre Service line
+        pygame.draw.rect(self.court_surface, WHITE, (20, 35, 360, 635), 5)  # Court boundary
+        pygame.draw.line(self.court_surface, WHITE, (90, 35), (90, 669.5), 5)  # Singles Sideline
+        pygame.draw.line(self.court_surface, WHITE, (310, 35), (310, 669.5), 5)  # Singles Sideline
+        # pygame.draw.line(self.court_surface, WHITE, (20, 352.5), (380, 352.5), 5) # mid line
+        pygame.draw.line(self.court_surface, WHITE, (90, 193.75), (310, 193.75), 5)  # Service line
+        pygame.draw.line(self.court_surface, WHITE, (90, 511.25), (310, 511.25), 5)  # Service line
+        pygame.draw.line(self.court_surface, WHITE, (200, 193.75), (200, 511.25), 5)  # Centre Service line
 
     def create_net(self):
         # Build the net
-        for x in range(0, 400, 5):  # Vertical lines
-            pygame.draw.line(self.court_surface, GRAY, (x, 270), (x, NET_HEIGHT), 1)
-        for y in range(270, NET_HEIGHT, 3):  # Horizontal lines
-            pygame.draw.line(self.court_surface, GRAY, (0, y), (400, y), 1)
-        pygame.draw.line(self.court_surface, DARK_GRAY, (0, 270), (400, 270), 5)  # Top net border
-        pygame.draw.line(self.court_surface, DARK_GRAY, (0, 270), (0, NET_HEIGHT), 7)  # Left net border
-        pygame.draw.line(self.court_surface, DARK_GRAY, (400, 270), (400, NET_HEIGHT), 7)  # Right net border
+        for x in range(20, 380, 5):  # Vertical lines
+            pygame.draw.line(self.court_surface, GRAY, (x, 330), (x, NET_HEIGHT), 1)
+        for y in range(330, NET_HEIGHT, 3):  # Horizontal lines
+            pygame.draw.line(self.court_surface, GRAY, (20, y), (380, y), 1)
+        pygame.draw.line(self.court_surface, DARK_GRAY, (20, 330), (380, 330), 5)  # Top net border
+        pygame.draw.line(self.court_surface, DARK_GRAY, (20, 330), (20, NET_HEIGHT+20), 7)  # Left net border
+        pygame.draw.line(self.court_surface, DARK_GRAY, (380, 330), (380, NET_HEIGHT+20), 7)  # Right net border
+
+    def hit_position(self, player):
+        if ((player.rect.x - self.ball.rect.x) < 0) and abs((player.rect.x - self.ball.rect.x) <= 10):
+            return ("Right", "Close")
+        elif ((player.rect.x - self.ball.rect.x) < 0) and abs((player.rect.x - self.ball.rect.x) < 15):
+            return ("Right", "Medium")
+        elif ((player.rect.x - self.ball.rect.x) < 0) and abs((player.rect.x - self.ball.rect.x) >= 15):
+            return ("Right", "Medium")
+        elif ((player.rect.x - self.ball.rect.x) > 0) and abs((player.rect.x - self.ball.rect.x) <= 10):
+            return ("Left", "Close")
+        else:
+            return ("Left", "Medium/Far")
+
+
+
 
     def run(self):
         while True:
@@ -105,21 +114,32 @@ class Game:
             # self.ball.show(self.court, self.ball.rect)
 
             # Update player position and draw it
-
             # self.user.move()
             # self.user.show(self.court, self.user.rect)
 
             # self.other.move()
             # self.other.show(self.court, self.other.rect)
 
+            # # Collision detection and ball interaction
+            # if self.user.rect.colliderect(self.ball.rect):
+            #     self.ball.chk_collision(self.user)
+            #     user_hit_pos, user_hit_dist = self.hit_position(self.user)
+            #     user_shot_intensity = self.user.chk_collision(user_hit_pos, user_hit_dist)
+            #     self.ball.ball_hit(user_shot_intensity, user_hit_pos)
 
+            # if self.other.rect.colliderect(self.ball.rect):
+            #     self.ball.chk_collision(self.other)
+            #     other_hit_pos, other_hit_dist = self.hit_position(self.other)
+            #     other_shot_intensity = self.other.chk_collision(other_hit_pos, other_hit_dist)
+            #     self.ball.ball_hit(other_shot_intensity, other_hit_pos)
 
             # Draw court on the screen
-            self.screen.blit(self.court, (200, 50))
+            self.screen.blit(self.court, (200, 0))
 
             # Update the display
             pygame.display.flip()
             self.clock.tick(60)
+
 
 
 # Run the game
--- scripts/entities.py
+++ scripts/entities.py
@@ -3,15 +3,15 @@ import pygame
 
 class Player:
 
-    def __init__(self,  screen, side="left", state="idle", left=500, top=460, width=50, height=50):
+    def __init__(self,  screen, player, side="left", state="idle", left=500, top=460, width=50, height=50):
         self.preview_list = []
         self.rect = pygame.Rect(left, top, width, height) #rectangle object
         self.position = [left, top]
         self.velocity = [0, 0] # X and Y velocity
         self.state = state # idle, moving, swinging racket
-        self.power = 0 #hit strength
         self.side = side # side is the side of the court (left/right) where userP will stand at first
         self.show(screen=screen, player_rect=self.rect)
+        self.player = player
 
     def show(self, screen, player_rect, color=(255, 0, 0)):
         # draw the animated character on the screen
@@ -19,20 +19,39 @@ class Player:
     
     def move_to_centre(self):
         # Move the ball towards the center (x = 400)
-        if self.position[0] < 175:
-            self.velocity[0] = 1  # Speed of movement towards the center
-        elif self.position[0] > 175:
-            self.velocity[0] = -1
-        else:
-            self.velocity[0] = 0  # Stop if at center
+        if (self.position[0] < 200):
+            self.velocity[0] = 5  # Speed of movement towards the center
+        elif self.position[0] > 0:
+            self.velocity[0] = -5
 
         # Update position with velocity
         self.position[0] += self.velocity[0]
         self.rect.x = self.position[0]
 
-    def move(self):
+    def run_towards_ball(self, ball):
         # Call move_to_centre for x-axis and implement any y-axis movement logic
         self.move_to_centre()
+        ball_x_pos = ball.rect.x
+        player_x_pos = self.rect.x
+        distance = (ball_x_pos - player_x_pos)
+        if abs(distance) > 0:
+            self.velocity[0] = max(min(distance // abs(distance), 5), -5)*5  # Move towards the ball
+        else:
+            self.velocity[0] = 0  # Stop if close enough
+
+        # Update position using the velocity
+        self.position[0] += self.velocity[0]
+        self.rect.x = self.position[0]
+
+    def reset(self):
+        #the function will run when player misses the ball
+        if self.player == "user":
+            self.position[0] = 0
+        else:
+            self.position[0] = 380
+        
+        self.rect.x = self.position[0]
+    
 
     def chk_collision(self, pos, x_dist):
         # a feature should be here for the hit strength
@@ -43,23 +62,26 @@ class Player:
             "Right" + "Medium": "Medium",
             "Right" + "Far": "Low",
             "Left" + "Close": "Medium",
-            "Left" + "Medium": "Low",
+            "Left" + "Medium/Far": "Low",
             "Left" + "Far": "Low"
         }
 
         shot = intensity_map[pos + x_dist]
 
         if shot == "High":
-            return (5, 10) #velocity at x and y
+            return (3, 20) #velocity at x and y
         elif shot == "Medium":
-            return (3, 7) #velocity at x and y
+            return (2, 14) #velocity at x and y
         else: #shot == "Low"
-            return (2, 4) #velocity at x and y
-
+            return (1, 10) #velocity at x and y
 
 
     def update(self):
         # update to true state either left side or right side of the court 
+        pass
+
+
+    def serve(self, ball):
         pass
 
 
@@ -79,35 +101,30 @@ class Ball:
         # self.create_ground(self.screen, ball_rect.x, ball_rect.y, ball_rect.width, ball_rect.height)
         pygame.draw.rect(screen, color, ball_rect)
 
-    def move_bounce(self):
-    # Reverse direction when hitting the top or bottom boundaries
-        if (self.position[1] <= 0)  or (self.position[1] >= 560):  # Top boundary # Bottom boundary
-            self.velocity[1] *= -1  # Move downwards # Move upwards 
+    # def move_bounce(self):
+    # # Reverse direction when hitting the top or bottom boundaries
+    #     if (self.position[1] <= 0)  or (self.position[1] >= 680):  # Top boundary # Bottom boundary
+    #         self.velocity[1] *= -1  # Move downwards # Move upwards 
 
-        # Update position with velocity
+    #     # Update position with velocity
+    #     self.position[1] += self.velocity[1]
+    #     # Update rect position for rendering
+    #     self.rect.y = self.position[1]
+    
+    def ball_hit(self, vel, direction):
+        # Set velocity based on hit intensity and direction
+        x_v, y_v = vel
+        if direction == "Right":
+            self.velocity = [x_v, -y_v]  # Ball moves to the right and upward/downward
+        else:  # direction == "Left"
+            self.velocity = [-x_v, -y_v]  # Ball moves to the left and upward/downward
+        
+        # Update ball's position with new velocity
+        self.position[0] += self.velocity[0]
         self.position[1] += self.velocity[1]
-        # Update rect position for rendering
+        self.rect.x = self.position[0]
         self.rect.y = self.position[1]
     
-    def move_back(self, x_vel, y_vel):
-        pass
-
-
-    def move(self):
-        # Call move_to_centre for x-axis and implement any y-axis movement logic
-        self.move_bounce()
-
-        # Implement other movement behavior here (e.g., if the ball needs to move vertically)
-        # if self.state == "moving":
-        #     self.position[1] += self.velocity[1]  # Adjust this to your needs
-        #     self.rect.y = self.position[1]
-
-    def swing(self):
-        pass
-
-    def chk_collision(self):
-        # Add logic for collision detection here
-        pass
 
     def update(self):
         # Update ball's position and state
